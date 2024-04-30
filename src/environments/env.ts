6f41666d9cfc4916d4d5bcb6ab8c391086255246
Raul <cado.rfmr@gmail.com>
2024-04-30 16:15:44 -0400

    FR-9 Cambios

--- src/environments/env.ts
+++ src/environments/env.ts
@@ -1,5 +1,5 @@
 export const env = {
-    //API_URL : 'https://cub-balanced-slowly.ngrok-free.app/api/'
+    //API_URL : 'https://annually-certain-joey.ngrok-free.app/api/',
     API_URL : 'http://localhost:8000/api/',
     GOOGLE_REDIRECT_URL : 'http://localhost:8000/login/google/redirect'
 }
