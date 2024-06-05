14a0548186bf9ff0907f4165399318ebcab11499
Valentin Salgado <valentin.salgadog@gmail.com>
2024-06-05 13:36:03 -0400

    Arreglos menores v3

--- src/environments/env.ts
+++ src/environments/env.ts
@@ -1,6 +1,6 @@
 export const env = {
     API_URL : 'http://localhost:8000/api/',
     //NO USAR, NO ESTA IMPLEMENTADO
-    GOOGLE_REDIRECT_URL : 'http://localhost:8000/login/google/redirect',
-    GOOGLE_CALLBACK_URL : 'http://localhost:8000/login/google/callback'
+    GOOGLE_REDIRECT_URL : 'http://localhost:8000/google/redirect',
+    GOOGLE_CALLBACK_URL : 'http://localhost:8000/google/callback'
 }
